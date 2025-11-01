5e78d4aec9212851bb44735ba5e7b0e489608894
Simon Jürgensmeyer <6493966+sj14@users.noreply.github.com>
2025-11-01 16:51:39 +0100

    Add: Get Object ACL

---
+++ controller/object_acl_get.go
@@ -0,0 +1,30 @@
+package controller
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/sj14/sss/util"
+)
+
+func (c *Controller) ObjectACLGet(bucket, key, version string) error {
+	resp, err := c.client.GetObjectAcl(c.ctx, &s3.GetObjectAclInput{
+		Bucket:    aws.String(bucket),
+		Key:       aws.String(key),
+		VersionId: util.IfNotZero(version),
+	})
+	if err != nil {
+		return err
+	}
+
+	b, err := json.MarshalIndent(resp, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(b))
+
+	return nil
+}
--- util/zero.go
+++ util/zero.go
@@ -1,5 +1,13 @@
 package util
 
+func IfNotZero[T comparable](value T) *T {
+	var zero T
+	if value != zero {
+		return &value
+	}
+	return nil
+}
+
 func SetIfNotZero[T comparable](dst **T, value T) {
 	var zero T
 	if value != zero {
