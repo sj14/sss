866b5e53ecb603813f60863e6594877e2a893cd7
Simon Jürgensmeyer <6493966+sj14@users.noreply.github.com>
2025-12-07 10:49:41 +0100

    Add more kong

--- controller/bucket_delete.go
+++ controller/bucket_delete.go
@@ -7,9 +7,12 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-func (c *Controller) BucketDelete(bucket string) error {
+func (c *Controller) BucketDelete(bucket string, flagForce bool) error {
 	if bucket == "" {
 		return fmt.Errorf("no bucket name specified")
+	}
+	if !flagForce {
+		return fmt.Errorf("force flag required")
 	}
 	_, err := c.client.DeleteBucket(c.ctx, &s3.DeleteBucketInput{
 		Bucket: aws.String(bucket),
