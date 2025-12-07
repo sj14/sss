6a313e4e45e3355dfb5d9f47e3f46ba058a34b29
Simon Jürgensmeyer <6493966+sj14@users.noreply.github.com>
2025-12-07 10:49:23 +0100

    Adjust multipart behaviour

--- controller/bucket_multipart_parts.go
+++ controller/bucket_multipart_parts.go
@@ -4,24 +4,43 @@ import (
 	"encoding/json"
 	"fmt"
 	"iter"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/dustin/go-humanize"
 )
 
-func (c *Controller) BucketPartsList(bucket, key, uploadID string) error {
+func (c *Controller) BucketPartsList(bucket, key, uploadID string, asJson bool) error {
+	if key == "" {
+		return fmt.Errorf("empty key")
+	}
+	if uploadID == "" {
+		return fmt.Errorf("empty upload ID")
+	}
+
 	for part, err := range c.bucketPartsList(bucket, key, uploadID) {
 		if err != nil {
 			return err
 		}
 
-		b, err := json.MarshalIndent(part, "", "  ")
-		if err != nil {
-			return err
+		if asJson {
+			b, err := json.MarshalIndent(part, "", "  ")
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(string(b))
+			continue
 		}
 
-		fmt.Println(string(b))
+		fmt.Printf("%s  #%d  %8s  %s\n",
+			part.LastModified.Local().Format(time.DateTime),
+			*part.PartNumber,
+			humanize.IBytes(uint64(*part.Size)),
+			*part.ETag,
+		)
 	}
 
 	return nil
